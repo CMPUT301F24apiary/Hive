bb3fd0ac68cd4e0da59323281419f82a4556bc87
Hrittija Bhattacharjee <hrittija2001@gmail.com>
2024-11-02 07:25:20 -0600

    Create/Edit facility activity

--- app/src/main/java/com/example/hive/OrganizerEventListActivity.java
+++ app/src/main/java/com/example/hive/OrganizerEventListActivity.java
@@ -1,15 +1,3 @@
-/**
- * EventListActivity.java
- *
- * This activity displays a list of events to the user. It includes an ImageButton that allows
- * the user to navigate to their profile page.
- *
- * <p>Outstanding Issues:
- * - None at this time.</p>
- *
- * @author Dina
- * @version 1.0
- */
 package com.example.hive;
 
 import android.content.Intent;
@@ -21,13 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class OrganizerEventListActivity extends AppCompatActivity {
 
     private ImageButton facilityprofileButton;
-
-    /**
-     * Called when the activity is starting. This is where most initialization should be done.
-     * It sets up the UI and initializes the profile button.
-     *
-     * @param savedInstanceState If the activity is being re-initialized after previously being shut down, this Bundle contains the data it most recently supplied in onSaveInstanceState. Otherwise, it is null.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
